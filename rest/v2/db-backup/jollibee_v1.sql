b3d4c19c14bf39132a8958f8ae4e48fef092d3a6
juanillo <juanillojohnmichael7@gmail.com>
2024-12-11 15:57:13 +0800

    updated

--- rest/v2/db-backup/jollibee_v1.sql
+++ rest/v2/db-backup/jollibee_v1.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 11, 2024 at 05:01 AM
+-- Generation Time: Dec 11, 2024 at 08:55 AM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.2.0
 
@@ -30,6 +30,7 @@ SET time_zone = "+00:00";
 CREATE TABLE `jollibee_category` (
   `category_aid` int(11) NOT NULL,
   `category_is_active` tinyint(1) NOT NULL,
+  `category_image` varchar(20) NOT NULL,
   `category_title` varchar(30) NOT NULL,
   `category_datetime` varchar(20) NOT NULL,
   `category_created` int(11) NOT NULL
@@ -39,12 +40,15 @@ CREATE TABLE `jollibee_category` (
 -- Dumping data for table `jollibee_category`
 --
 
-INSERT INTO `jollibee_category` (`category_aid`, `category_is_active`, `category_title`, `category_datetime`, `category_created`) VALUES
-(7, 1, 'Chicken', '2024-12-10 15:09:22', 2024),
-(8, 1, 'Burger', '2024-12-10 15:09:27', 2024),
-(9, 1, 'Spaghetti', '2024-12-10 15:09:35', 2024),
-(10, 1, 'Palabok', '2024-12-11 09:07:09', 2024),
-(11, 1, 'Sides', '', 2024);
+INSERT INTO `jollibee_category` (`category_aid`, `category_is_active`, `category_image`, `category_title`, `category_datetime`, `category_created`) VALUES
+(7, 1, 'nav-chickenjoy.jpg', 'Chickenjoy', '2024-12-11 15:24:59', 2024),
+(8, 1, 'nav-burger.webp', 'Burger', '2024-12-11 14:23:07', 2024),
+(9, 1, 'nav-spaghetti.webp', 'Spaghetti', '2024-12-11 14:23:26', 2024),
+(10, 1, 'nav-palabok.webp', 'Palabok', '2024-12-11 14:23:57', 2024),
+(11, 1, 'nav-sides.webp', 'Sides', '2024-12-11 14:24:05', 2024),
+(12, 1, 'nav-value-meal.webp', 'Value Meal', '2024-12-11 14:33:19', 2024),
+(13, 1, 'nav-desserts.webp', 'Dessert', '2024-12-11 15:06:25', 2024),
+(14, 1, 'steak-1.webp', 'Burger Steak', '2024-12-11 15:22:27', 2024);
 
 -- --------------------------------------------------------
 
@@ -68,11 +72,32 @@ CREATE TABLE `jollibee_food` (
 --
 
 INSERT INTO `jollibee_food` (`food_aid`, `food_is_active`, `food_image`, `food_title`, `food_price`, `food_category_id`, `food_datetime`, `food_created`) VALUES
-(2, 1, '', 'YumBurger Solo', 65, 8, '', 0),
-(3, 0, '', 'Test', 0, 7, '', 2024),
-(4, 1, '', 'test2', 245, 8, '', 0),
-(5, 1, '', 'test4', 45, 8, '2024-12-11 09:38:00', 2024),
-(7, 1, '', 'asd', 0, 0, '2024-12-11 09:44:38', 2024);
+(9, 1, 'chicken-1.webp', '2pc ChickenJoy', 200, 7, '2024-12-11 15:34:00', 2024),
+(10, 1, 'chicken-2.webp', '3pc Chicken Joy', 280, 7, '2024-12-11 15:35:09', 2024),
+(11, 1, 'chicken-3.webp', '2pcs Fries, bucket of chicken', 300, 7, '2024-12-11 15:36:16', 2024),
+(12, 1, 'burger-1.webp', 'YumBurger', 40, 8, '2024-12-11 15:36:37', 2024),
+(13, 1, 'burger-2.webp', 'YumBurger w/ Fries', 60, 8, '2024-12-11 15:36:53', 2024),
+(14, 1, 'burger-4.webp', 'Cheezy YumBurger w/ Fries', 70, 8, '2024-12-11 15:37:26', 2024),
+(15, 1, 'spag-1.webp', 'Jollly Spaghetti Solo', 75, 9, '2024-12-11 15:38:04', 2024),
+(16, 1, 'spag-2.webp', 'Jolly Spaghetti w/ Chicken', 180, 9, '2024-12-11 15:38:28', 2024),
+(17, 1, 'spag-3.webp', 'Family Pan Spaghetti', 280, 9, '2024-12-11 15:38:49', 2024),
+(18, 1, 'palabok-1.webp', 'Palabok Solo', 180, 10, '2024-12-11 15:39:11', 2024),
+(19, 1, 'palabok-2.webp', 'Palabok w/ drinks', 205, 10, '2024-12-11 15:39:35', 2024),
+(20, 1, 'palabok-3.webp', 'Palabok Family Pan', 350, 10, '2024-12-11 15:39:58', 2024),
+(21, 1, 'sides-1.webp', 'Jolly Crispy Fries', 50, 11, '2024-12-11 15:40:20', 2024),
+(22, 1, 'sides-2.webp', 'Mashed Potato', 50, 11, '2024-12-11 15:40:43', 2024),
+(23, 1, 'sides-3.webp', 'Rice', 35, 11, '2024-12-11 15:41:04', 2024),
+(24, 1, 'sides-4.webp', 'Adobo Rice', 45, 11, '2024-12-11 15:41:25', 2024),
+(25, 1, 'sides-5.webp', 'Jolly Spaghetti ismol', 90, 11, '2024-12-11 15:41:51', 2024),
+(26, 1, 'value-meal-1.webp', 'Chicken Burger w/ Fries', 180, 12, '2024-12-11 15:42:21', 2024),
+(27, 1, 'value-meal-2.webp', 'Cheeze Chicken Burger', 200, 12, '2024-12-11 15:42:45', 2024),
+(28, 1, 'value-meal-3.webp', '2pc chicken w/ peach mango', 150, 12, '2024-12-11 15:43:11', 2024),
+(29, 1, 'steak-1.webp', 'Burger Steak Solo', 55, 14, '2024-12-11 15:43:48', 2024),
+(30, 1, 'steak-2.webp', 'Family Pan Burger Steak', 250, 14, '2024-12-11 15:44:07', 2024),
+(31, 1, 'steak-3.webp', 'Family Pan Chicken and Steak', 350, 14, '2024-12-11 15:44:47', 2024),
+(32, 1, 'dessert-1.webp', 'Peach Mango Pie', 35, 13, '2024-12-11 15:48:25', 2024),
+(33, 1, 'dessert-2.webp', 'Ube Pie', 30, 13, '2024-12-11 15:48:42', 2024),
+(34, 1, 'dessert-3.webp', '3pcs Peach Mango Pie', 150, 13, '2024-12-11 15:49:07', 2024);
 
 --
 -- Indexes for dumped tables
@@ -98,13 +123,13 @@ ALTER TABLE `jollibee_food`
 -- AUTO_INCREMENT for table `jollibee_category`
 --
 ALTER TABLE `jollibee_category`
-  MODIFY `category_aid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `category_aid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT for table `jollibee_food`
 --
 ALTER TABLE `jollibee_food`
-  MODIFY `food_aid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `food_aid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
